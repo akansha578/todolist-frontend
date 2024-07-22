c8bafca652401afd237cb1c3ea67b72a01122d28
akansha578 <akanshamishra9562@gmail.com>
2024-07-22 20:02:56 +0530

    todolist-frontend with different routes

---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home(){
+  return (
+    <div className="home-page">
+      <h1>Todo List</h1>
+      <p>Organize your tasks efficiently.</p>
+    </div>
+  );
+}
+
+export default Home;
